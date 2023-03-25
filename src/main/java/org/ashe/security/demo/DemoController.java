9b683dfb8a0dde80f13dde70022fc74cd931eddb
ashe <1414547891@qq.com>
2023-03-25 21:54:51 +0800

    feat: jdk17+spring-security 6+jwt

---
+++ src/main/java/org/ashe/security/demo/DemoController.java
@@ -0,0 +1,17 @@
+package org.ashe.security.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("hello from secured endpoint");
+    }
+}
---
+++ src/main/java/org/ashe/security/user/Role.java
@@ -0,0 +1,7 @@
+package org.ashe.security.user;
+
+public enum Role {
+
+        USER,
+        ADMIN
+}
